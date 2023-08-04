6bf64b845b7723c09ba7e1c458910aa72585e629
Dhrubajyoti Biswas <dsofthelp.org@outlook.com>
2023-08-04 20:37:57 +0530

    Update whimsy.py
    Co-Authored-By: Moumita Biswas <122099970+Moumitaengineer@users.noreply.github.com>
    Co-Authored-By: Subrata Biswas <98461651+Dhrubajyo@users.noreply.github.com>

---
+++ whimsy.py
@@ -0,0 +1,5 @@
+dict_computer = {
+    'A_comp': {
+        'App': "An app, which is short for application, is a type of software that can be installed and run on a computer, tablet, smartphone or other electronic devices."
+    }
+}
