84d3a089a2f730b7dcd471fcb63b47f270255ff5
Dhrubajyoti Biswas <dsofthelp.org@outlook.com>
2023-08-04 20:45:52 +0530

    Update whimsy.py
    Co-Authored-By: Moumita Biswas <122099970+Moumitaengineer@users.noreply.github.com>
    Co-Authored-By: Subrata Biswas <98461651+Dhrubajyo@users.noreply.github.com>

--- whimsy.py
+++ whimsy.py
@@ -1,6 +1,6 @@
 dict_computer = {
     'A_comp': {
         'App': "An app, which is short for application, is a type of software that can be installed and run on a computer, tablet, smartphone or other electronic devices.",
-        'Accessibility': "Accessibility is the concept of whether a product or service can be used by everyone—however they encounter it"
+        'Accessibility': "Accessibility is the concept of whether a product or service can be used by everyone—however they encounter it."
     }
 }
