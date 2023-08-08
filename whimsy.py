0879c8313d0c6629baf39ae8c69b5bd7f9f8008e
Dhrubajyoti Biswas <dsofthelp.org@outlook.com>
2023-08-08 16:18:09 +0530

    Update whimsy.py

--- whimsy.py
+++ whimsy.py
@@ -1,6 +1,25 @@
-dict_computer = {
-    'A_comp': {
+dict = {
         'App': "An app, which is short for application, is a type of software that can be installed and run on a computer, tablet, smartphone or other electronic devices",
-        'Accessibility': "Accessibility is the concept of whether a product or service can be used by everyone—however they encounter it."
-    }
+        'Accessibility': "Accessibility is the concept of whether a product or service can be used by everyone—however they encounter it.",
+        'Adapter': "An adapter is a device that allows a specific type of hardware to work with another device that would otherwise be incompatible",
+        'Abstraction': "An abstraction is a general concept or idea, rather than something concrete or tangible.",
+        'Analog':"Analog is an adjective that describes a continuous measurement or transmission of a signal. It is often contrasted with digital, which is how computers store and process data using ones and zeros.",
+        'Antivirus':"Antivirus software is a type of utility used for scanning and removing viruses from your computer.",
+        'ASCII':"Stands for `American Standard Code for Information Interchange`. ASCII is a character encoding that uses numeric codes to represent characters. These include upper and lowercase English letters, numbers, and punctuation symbols.",
+        'Autofill':"Autofill is a software function that automatically enters data in web forms and spreadsheets.",
+        'Autocorrect':"Autocorrect is a software feature that corrects misspellings as you type.",
+        'Avatar':"An avatar specifically refers to a character that represents an online user"
+
 }
+
+# sorted_dict_computer = sorted(dict.keys())
+for keys in sorted(dict.keys()):
+    print(keys)
+
+print(":Options are present below:")
+option = input("Write your option: ")
+def search_word():
+    search_key = print("Search")
+    if option == "search_word":
+        word_search = dict.get(input("Enter the word:\n"))
+search_word()
